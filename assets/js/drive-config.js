c52510ffe7f2ceb0086ec2666858ceb10aa70371
Elis Poudel <poudelelis@gmail.com>
2026-08-23 01:52:05 +0545

    new update admin pannel

---
+++ assets/js/drive-config.js
@@ -0,0 +1,7 @@
+
+const DRIVE_CONFIG = {
+  enabled: true,
+  webAppUrl: 'https://script.google.com/macros/s/AKfycby9weUx9-nYWoGt3M3flghz5FS-dm3utvqmHmjg_nftLLwmFh0q7Yn-yMi_U8lLn7ANpg/exec',
+  uploadSecret: 'pbsupdate123',
+};
+
