7aa6dc062f20238b373608478786d2370f1199f0
Elis Poudel <poudelelis@gmail.com>
2026-08-23 15:23:45 +0545

    update 5.0

---
+++ assets/js/drive-config.js
@@ -0,0 +1,7 @@
+
+const DRIVE_CONFIG = {
+  enabled: true,
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbyphkPyVBZkVsWEwEUoKhpC8Sxv8-pXS7ugytUUA8qtwe9o6KJF53Fw9u2svPhnuUz-Bw/exec',
+  uploadSecret: 'pbsupdate123',
+};
+
