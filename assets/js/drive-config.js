ea28436abb31695b1ffb3c830eafa54869403b44
Elis Poudel <poudelelis@gmail.com>
2026-09-15 23:24:27 +0545

    Delete assets directory

--- assets/js/drive-config.js
+++
@@ -1,7 +0,0 @@
-
-const DRIVE_CONFIG = {
-  enabled: true,
-  webAppUrl: 'https://script.google.com/macros/s/AKfycbzIbg4Zul-JW7ELK1WNJi1LqhwiveLDx1OI8rc3kQMMxX8_YMDpQMzzDRr6XNjhChIBSw/exec',
-  uploadSecret: 'k7v2m9x4pa8r3d6q',
-};
-
